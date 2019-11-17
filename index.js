6d27e5195ba78cb6ebcfe2e601c8e0509e1584fb
gohandrogo <alondono11@gmail.com>
2019-11-17 14:23:59 -0500

    refactoring and fixing some bugs centered around the paginator limits?

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+var paginator = null;
+var filter = new Filter();
+var sortedState = {
+  isSorted: null,
+  sortType: '',
+};
+
+function fetchOpenTrivia() {
+  window.fetch('https://opentdb.com/api.php?amount=25')
+    .then(function(response){
+      return response.json();
+    }).then(function(json){
+      paginator = new Paginator(json.results, 5);
+      paginator.totalPageNumberElement.innerHTML = paginator.pages;
+      paginator.pageNumberElement.innerHTML = paginator.currentPage+1;
+      console.log(json.results);
+      var page = paginator.showResultsOnPage(0, paginator.results);
+      formatPage(page);
+      addEventListeners(paginator)
+  }
+  );
+}  
+
 function formatPage(page){
   paginator.content.innerHTML = '';
   for(var i = 0; i < page.length; i++){
@@ -8,44 +31,87 @@ function formatPage(page){
         <p class="category">Category ${page[i].category}</p>
         <p class="difficulty">difficulty ${page[i].difficulty}</p>
         <p class="type">type ${page[i].type}</p>
-        <p class="question">question <b>${page[i].question}</b></p>
+        <p class="question">question <b><a href="${googleHref(page[i].question)}">${page[i].question}</a></b></p>
         </div>
         `
       this.content.appendChild(item);
   }
 }
-var sortedState = {
-  isSorted: null,
-  sortType: '',
+
+function googleHref (query) {
+  return `https://www.google.com/search?q=${query.split(' ').join('+')}`
+}
+
+function Paginator(results, resultsPerPage){
+  this.results = results;
+  this.resultsPerPage = resultsPerPage;
+  this.pages = results.length / resultsPerPage;
+  this.currentPage=0;
+  this.previousButton = document.getElementById('previous-page');
+  this.nextButton = document.getElementById('next-page');
+  this.content = document.getElementById('content');
+  this.pageNumberElement = document.getElementById('current-page');
+  this.totalPageNumberElement = document.getElementById('total-pages');
+  
+}
+
+Paginator.prototype.showResultsOnPage = function(number, results){
+  var start = number * this.resultsPerPage;
+  return results.slice(start, start + this.resultsPerPage);
 };
-var paginator = null;
-var filter = new Filter();
-function fetchOpenTrivia() {
-  window.fetch('https://opentdb.com/api.php?amount=30')
-    .then(function(response){
-      return response.json();
-    }).then(function(json){
-      paginator = new Paginator(json.results, 5);
-      console.log(json.results);
-      var page = paginator.showResultsOnPage(0, paginator.results);
-      formatPage(page);
-      addEventListeners(paginator)
+
+
+function updatePageNumber(paginator) {
+  paginator.pageNumberElement.innerHTML = paginator.currentPage+1;
+}
+
+function Filter(){
+  this.toggleCategory = document.getElementById('group-by-category');
+  this.toggleDifficulty = document.getElementById('group-by-difficulty');
+  this.toggleType = document.getElementById('group-by-type');
+}
+
+function group(property, collection){
+  var val, index, finalArray = [];
+        values = [], result = [];
+  function reduceGroups(total, current, index, arr){
+    if(index === 0){
+      return current;
+    } else {
+      return total.concat(current);
+    }
+  
   }
-  );
-}  
+  for (var i = 0; i < collection.length; i++) {
+      val = collection[i][property];
+      index = values.indexOf(val);
+      if (index > -1){
+          result[index].push(collection[i]);
+      }else {
+          values.push(val);
+          result.push([collection[i]]);
+      }
+  }
+  finalArray = result.reduce(reduceGroups, []);
+
+  return finalArray;
+};
+
 function addEventListeners(paginator) {
   paginator.previousButton.addEventListener('click', function(){
-    if(paginator.currentPage+1 > 0){
+    if(paginator.currentPage > 0){
       paginator.currentPage--;
       var newPage = paginator.showResultsOnPage(paginator.currentPage, paginator.results);
       formatPage(newPage);
+      updatePageNumber(paginator, 0)
     }
   })
   paginator.nextButton.addEventListener('click', function(){
-    if(paginator.currentPage <= paginator.pages){
+    if(paginator.currentPage+1 < paginator.pages){
       paginator.currentPage++;
       var newPage = paginator.showResultsOnPage(paginator.currentPage, paginator.results);
       formatPage(newPage);
+      updatePageNumber(paginator)
     }
   })
   filter.toggleCategory.addEventListener('click', function(){
@@ -80,54 +146,4 @@ function addEventListeners(paginator) {
   })
 }
 
-function Paginator(results, resultsPerPage){
-  this.results = results;
-  this.resultsPerPage = resultsPerPage;
-  this.pages = results.length / resultsPerPage;
-  this.currentPage=1;
-  this.previousButton = document.getElementById('previous-page');
-  this.nextButton = document.getElementById('next-page');
-  this.content = document.getElementById('content');
-}
-
-function Filter(){
-  this.toggleCategory = document.getElementById('group-by-category');
-  this.toggleDifficulty = document.getElementById('group-by-difficulty');
-  this.toggleType = document.getElementById('group-by-type');
-}
-
-Paginator.prototype.showResultsOnPage = function(number, results){
-  console.log(number)
-  var start = number * this.resultsPerPage;
-  return results.slice(start, start + this.resultsPerPage);
-};
 fetchOpenTrivia();
-
-
-function reduceGroups(total, current, index, arr){
-  if(index === 0){
-    return current;
-  } else {
-    return total.concat(current);
-  }
-
-}
-
-function group(property, collection){
-  var val, index, finalArray = [];
-        values = [], result = [];
-    for (var i = 0; i < collection.length; i++) {
-        val = collection[i][property];
-        index = values.indexOf(val);
-        if (index > -1){
-            result[index].push(collection[i]);
-        }else {
-            values.push(val);
-            result.push([collection[i]]);
-        }
-    }
-    finalArray = result.reduce(reduceGroups, []);
-
-    return finalArray;
-};
-
